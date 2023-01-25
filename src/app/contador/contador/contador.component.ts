dc00ea618314db65e8d79c10d4cda8e40cde21c8
Juan Manuel <juan.sanchez@fianly.com>
2023-01-25 14:35:45 -0500

    introduction

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,27 @@
+
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-contador',
+  template:`
+
+    <h1>{{ title }}</h1>
+    <h3>La base es: <strong>{{ base }}</strong></h3>
+
+    <button (click)="acumular(this.base)">{{ base }}</button>
+
+    <span>{{ numero }}</span>
+
+    <button (click)="acumular(-this.base)">-{{ base }}</button>
+
+  `
+})
+export class ContadorComponent {
+  title: string = 'Contador App';
+  numero: number = 10;
+  base:number = 10;
+
+  acumular(valor:number) {
+    this.numero += valor;
+  }
+}
